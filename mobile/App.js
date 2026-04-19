3bd5c4101d66c77a967d62bad681dc93305252e4
starrynighttech <starrynight8531@gmail.com>
2026-04-19 22:36:02 +0800

    Create App.js

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+
+import Home from "./screens/Home"
+import Shake from "./screens/Shake"
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Shake" component={Shake} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
